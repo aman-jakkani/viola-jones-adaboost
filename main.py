bec7006ed1527dad7124b8f9033b7fbc73b9036d
Aman Jakkani <aj280598@gmail.com>
2019-12-09 10:57:53 -0600

    remove cascader

--- main.py
+++ main.py
@@ -18,7 +18,8 @@ if __name__ == "__main__":
     non_faces_train_int = list(map(II.to_integral, non_faces_train))
     print('..done. ' + str(len(non_faces_train)) + ' non faces loaded.\n')
 
-    num_classifiers = 3
+    #number of rounds
+    num_classifiers = 1
     # For performance reasons restricting feature size
     min_feature_height = 6
     max_feature_height = 8
@@ -29,25 +30,25 @@ if __name__ == "__main__":
     classifiers = AB.learn(faces_train_int, non_faces_train_int, num_classifiers, min_feature_height, max_feature_height, min_feature_width, max_feature_width)
     for n in range(len(classifiers)):
         print(classifiers[n].type, classifiers[n].top_left, classifiers[n].width, classifiers[n].height, classifiers[n].threshold)
-    
-    print('Loading test faces..')
+
+    print('Loading test faces')
     faces_test = UT.load_images(pos_testing_path)
     faces_test_int = list(map(II.to_integral, faces_test))
-    print('..done. ' + str(len(faces_test)) + ' faces loaded.\n\nLoading test non faces..')
+    print(str(len(faces_test)) + ' faces loaded.\n\nLoading test non faces..')
     non_faces_test = UT.load_images(neg_testing_path)
     non_faces_test_int = list(map(II.to_integral, non_faces_test))
-    print('..done. ' + str(len(non_faces_test)) + ' non faces loaded.\n')
+    print(str(len(non_faces_test)) + ' non faces loaded.\n')
     
     print('Testing selected classifiers..')
     correct_faces = 0
     correct_non_faces = 0
-    correct_faces = sum(UT.ensemble_vote_all(faces_test_int, classifiers))
-    correct_non_faces = len(non_faces_test) - sum(UT.ensemble_vote_all(non_faces_test_int, classifiers))
+    correct_faces, FN, FP, correct_non_faces = UT.count_rate(faces_test_int, non_faces_test_int, classifiers)
 
     print('..done.\n\nResult:\n      Faces: ' + str(correct_faces) + '/' + str(len(faces_test))
           + '  (' + str((float(correct_faces) / len(faces_test)) * 100) + '%)\n  non-Faces: '
           + str(correct_non_faces) + '/' + str(len(non_faces_test)) + '  ('
           + str((float(correct_non_faces) / len(non_faces_test)) * 100) + '%)')
-    
-    cascader = C.cascaded_classifier(faces_train_int, non_faces_train_int, faces_test_int, non_faces_test_int)
-    C.test_cascade_classifier(faces_test_int, non_faces_test_int, cascader)
+    print('False Negative Rate: ' + str(FN) + '/' + str(len(faces_test))
+          + '  (' + str((float(FN) / len(faces_test)) * 100) + '%)\n  False Positive Rate: '
+          + str(FP) + '/' + str(len(non_faces_test)) + '  ('
+          + str((float(FP) / len(non_faces_test)) * 100) + '%)')
--- src/adaboost.py
+++ src/adaboost.py
@@ -6,23 +6,24 @@ from src.haarfeatures import HaarLikeFeature as haar
 from src.haarfeatures import feat_type
 from src.utils import *
 
+haar_feats = list()
+
 def _create_features(img_width, img_height, min_feat_width, max_feat_width, min_feat_height, max_feat_height):
     threshold = 0
-    # function to create all possible features, returned as list
-    haar_feats = list()
+    if(len(haar_feats) == 0):
     # iterate according to types of rectangle features
-    for each_feat in feat_type:
-        # min of feature width is set in feat_type enum
-        feat_start_width = max(min_feat_width, each_feat.value[0])
-        for feat_width in range(feat_start_width, max_feat_width, each_feat.value[0]):
-            # min of feature height is set in feat_type enum
-            feat_start_height = max(min_feat_height, each_feat.value[1])
-            for feat_height in range(feat_start_height, max_feat_height, each_feat.value[1]):
-                # scan the whole image with sliding windows
-                for i in range(img_width - feat_width):
-                    for j in range(img_height - feat_height):
-                        haar_feats.append(haar(each_feat, (i,j), feat_width, feat_height, threshold, 1)) 
-                        haar_feats.append(haar(each_feat, (i,j), feat_width, feat_height, threshold, -1)) 
+        for each_feat in feat_type:
+            # min of feature width is set in feat_type enum
+            feat_start_width = max(min_feat_width, each_feat.value[0])
+            for feat_width in range(feat_start_width, max_feat_width, each_feat.value[0]):
+                # min of feature height is set in feat_type enum
+                feat_start_height = max(min_feat_height, each_feat.value[1])
+                for feat_height in range(feat_start_height, max_feat_height, each_feat.value[1]):
+                    # scan the whole image with sliding windows
+                    for i in range(img_width - feat_width):
+                        for j in range(img_height - feat_height):
+                            haar_feats.append(haar(each_feat, (i,j), feat_width, feat_height, threshold, 1)) 
+                            haar_feats.append(haar(each_feat, (i,j), feat_width, feat_height, threshold, -1)) 
     return haar_feats
 
 def _get_feature_vote(feature, img):
